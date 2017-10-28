ab794fbd11ac58743f1b5d2297a87f0b43b2dcf2
Jackie Xu <jackie@jackiexu.com>
2017-10-28 12:02:16 +0200

    Remove setup to comply to Riddles platform logistics

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ from setuptools import (
     setup,
 )
 
-version = '0.0.1.dev0'
+from blub.bot import Bot
 
-setup(
-    name='blub',
-    description='A bot for The Game of Life and Death',
-    version=version,
-    packages=find_packages('.', exclude=('tests',)),
-)
+# version = '0.0.1.dev0'
+#
+# setup(
+#     name='blub',
+#     description='A bot for The Game of Life and Death',
+#     version=version,
+#     packages=find_packages('.', exclude=('tests',)),
+# )
 
 # Needed to run on Riddles platform.
-if __name__  == '__main__':
-    pass
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run()
