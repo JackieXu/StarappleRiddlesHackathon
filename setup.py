2d66cb3898b8252d2958cdc4b4bffea3f01fe4e0
Jackie Xu <jackie@jackiexu.com>
2017-10-28 12:08:44 +0200

    Added hashbang for Riddles Python3 compliancy

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
-from setuptools import (
-    find_packages,
-    setup,
-)
+#!/usr/bin/python3
 
 from blub.bot import Bot
-
-# version = '0.0.1.dev0'
-#
-# setup(
-#     name='blub',
-#     description='A bot for The Game of Life and Death',
-#     version=version,
-#     packages=find_packages('.', exclude=('tests',)),
-# )
 
 # Needed to run on Riddles platform.
 if __name__ == '__main__':
